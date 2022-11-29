69b4ec69d2ba60bcd058c09a94a588ea818e6796
ValeriyLiubchenko <valeriyliubchenko98@gmail.com>
2022-11-29 21:07:17 +0200

    +

--- config/database.php
+++ config/database.php
@@ -4,14 +4,14 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 
 $capsule->addConnection([
-'driver' => 'mysql',
-'host' => 'localhost',
-'database' => 'database',
-'username' => 'root',
-'password' => 'password',
-'charset' => 'utf8',
-'collation' => 'utf8_unicode_ci',
-'prefix' => '',
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'database',
+    'username' => 'root',
+    'password' => 'password',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
 ]);
 
 // Set the event dispatcher used by Eloquent models... (optional)
--- index.php
+++ index.php
@@ -1,7 +1,2 @@
 <?php
 
-
-//require_once __DIR__ . '/autoload.php';
-//require_once __DIR__ . '/config/database.php';
-
-echo 111;
