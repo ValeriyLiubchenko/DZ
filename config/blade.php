6dc0a5ce7a7639676993218e988f190ae6556771
ValeriyLiubchenko <valeriyliubchenko98@gmail.com>
2022-12-06 19:54:44 +0200

    with rand tags + ctrl alt l

---
+++ config/blade.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\View\FileViewFinder;
+use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\View\Engines\CompilerEngine;
+use Illuminate\Container\Container;
+use Illuminate\Events\Dispatcher;
+use Illuminate\View\Factory;
+use Illuminate\Http\Response;
+
+$engineResolver = new EngineResolver();
+$fileSystem = new Filesystem();
+$fileViewFinder = new FileViewFinder(
+    $fileSystem, [__DIR__ . '/../resources/views']
+);
+
+$compiler = new BladeCompiler($fileSystem, __DIR__ . '/../resources/cache');
+$engineResolver->register('blade', function () use ($compiler) {
+    return new CompilerEngine($compiler);
+});
+$dispatcher = new Dispatcher((new Container()));
+
+$blade = new Factory(
+    $engineResolver,
+    $fileViewFinder,
+    $dispatcher
+);
